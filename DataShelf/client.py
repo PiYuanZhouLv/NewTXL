6d98d5fc0d97b826d8c2d0f49842ee022bc599e1
YellowWind <santag114514@outlook.com>
2024-02-07 18:12:10 +0800

    写好了数据库的连接操作

--- DataShelf/client.py
+++ DataShelf/client.py
@@ -5,15 +5,26 @@ class client:
     def __init__(self,address):
         self.sock = socket.socket(socket.AF_INET)
         self.sock.connect(address)
-    def new():
+    def new(self):
         self.sock.send(json.dumps(['NEW']).encode())
-        
-    def get():
-        ...
-    def set():
-        ...
-    def serch():
-        ...    
+        return int(self.sock.recv(1024))
+    def get(self,page):
+        self.sock.send(json.dumps(["get",page]).encode())
+        return json.loads(self.sock.recv(1024).decode())
+    def set(cls,name):
+        class Inner:
+            def __init__(self, cmd=[]) -> None:
+                self.cmd = cmd
+            def __getitem__(self, item):
+                return type(self)(self.cmd+[item])
+            def __setitem__(self, item, value):
+                cls.sock.send(json.dumps(["set", name, self.cmd+[item], value]).encode())
+                cls.sock.recv(1024)
+        return Inner()
+
+    def search(self,page):
+        self.sock.send(json.dumps(["search",page]).encode())   
+        return json.loads(self.sock.recv(1024).decode())
 
 
 
